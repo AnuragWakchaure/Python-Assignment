97c9b2c94dc6801dbb434235f59f810aa832deb1
AnuragWakchaure <151008646+AnuragWakchaure@users.noreply.github.com>
2025-07-25 08:26:58 +0530

    Update Task-1.py

--- Assignment-5/Task-1.py
+++ Assignment-5/Task-1.py
@@ -1,1 +1,18 @@
 
+
+student_marks = {
+    "Alice": 85,
+    "Bob": 92,
+    "Charlie": 78,
+    "David": 88,
+    "Eva": 95
+}
+
+
+name = input("Enter the student's name: ")
+
+
+if name in student_marks:
+    print(f"{name}'s marks: {student_marks[name]}")
+else:
+    print(f"Student '{name}' not found.")
